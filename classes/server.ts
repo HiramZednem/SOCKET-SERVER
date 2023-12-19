97c57fe478ef0cdd22f36e4496144ec6fb80b021
Hiram Mendez <hirammendez000@gmail.com>
2023-12-18 23:04:38 -0600

    sockets added

--- classes/server.ts
+++ classes/server.ts
@@ -1,6 +1,8 @@
 
 import express from 'express'
 import { SERVER_PORT } from '../global/environment';
+import socketIO from "socket.io";
+import http from "http";
 
 
 export default class Server {
@@ -8,17 +10,33 @@ export default class Server {
     public app: express.Application;
     public port: number;
 
+    public io: socketIO.Server;
+    private httpServer: http.Server;
 
-    constructor( ) {
+
+    constructor() {
         
-        this.app = express()
+        this.app  = express()
         this.port = SERVER_PORT;
 
+        this.httpServer = new http.Server( this.app );
+        this.io = require('socket.io')(this.httpServer);
+        
+        this.escucharSockets()
+    }
+
+
+    private escucharSockets() {
+        console.log('Escuchando conexiones -> sockets');
+
+        this.io.on('connection', (cliente) => {
+            console.log('Nuevo cliente conectado');
+        })
     }
 
     start ( callback: () => void ) {
         
-        this.app.listen( this.port, callback );
+        this.httpServer.listen( this.port, callback );
 
     }
 }
